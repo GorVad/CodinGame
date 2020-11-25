57638dc85f81c8039823590776cba59b66eb7cc3
DaxterMann <48520987+DaxterMann@users.noreply.github.com>
2020-11-25 17:07:44 +0300

    Новые задачи

---
+++ CodinGame/Easy/HORSE-RACING DUALS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * URL: https://www.codingame.com/ide/puzzle/horse-racing-duals
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] mass = new int[N];
+        int lowest = 10000000;
+        int res;
+        for (int i = 0; i < N; i++)
+        {
+            int pi = int.Parse(Console.ReadLine());
+            mass[i] = pi;
+        }
+
+        Array.Sort(mass);
+
+        for (int i = 0; i < mass.Length - 1; i++)
+        {
+            res = Math.Abs(mass[i] - mass[i + 1]);
+            if (res < lowest) lowest = res;
+        }
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine(lowest);
+    }
+}
---
+++ CodinGame/Easy/TEMPERATURES.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ *URL: https://www.codingame.com/ide/puzzle/temperatures
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+        string[] inputs = Console.ReadLine().Split(' ');
+        int[] tempValues = new int[n];
+        int nearest = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            int t = int.Parse(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+            tempValues[i] = t;
+        }
+        if (tempValues.Length != 0)
+        {
+            foreach (int t in tempValues)
+            {
+                if (t == nearest) nearest = t;
+                else if (t == -nearest) nearest = Math.Abs(t);
+                else if (Math.Abs(t) < Math.Abs(nearest)) nearest = t;
+            }
+        }
+        else nearest = 0;
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine(nearest);
+    }
+}
