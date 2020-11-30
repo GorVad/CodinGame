aaefb136744004a99d6a57ff6fbbd9cfe18584af
DaxterMann <48520987+DaxterMann@users.noreply.github.com>
2020-11-30 10:46:32 +0300

    Новые задачи

---
+++ CodinGame/Easy/Create the longest sequence of 1s.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * URL: https://www.codingame.com/ide/puzzle/create-the-longest-sequence-of-1s
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string bit = Console.ReadLine();
+        int longest = int.MinValue;
+
+        string[] b = bit.Split('0');
+
+        for (int i = 0; i < b.Length - 1; i++)
+        {
+            longest = longest < b[i].Length + b[i + 1].Length + 1 ? b[i].Length + b[i + 1].Length + 1 : longest;
+        }
+        Console.WriteLine(longest);
+    }
+}
---
+++ CodinGame/Medium/Shadows of the Knight - Episode 1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int W = int.Parse(inputs[0]); // width of the building.
+        int H = int.Parse(inputs[1]); // height of the building.
+        int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
+        inputs = Console.ReadLine().Split(' ');
+        int X0 = int.Parse(inputs[0]);
+        int Y0 = int.Parse(inputs[1]);
+
+        int L = 0, R = W - 1;
+        int U = 0, D = H - 1;
+        // game loop
+        while (true)
+        {
+            string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+            if (bombDir.Contains("R")) L = X0 + 1;
+            else if (bombDir.Contains("L")) R = X0 - 1;
+
+            if (bombDir.Contains("U")) D = Y0 - 1;
+            else if (bombDir.Contains("D")) U = Y0 + 1;
+
+            X0 = (R + L) / 2;
+            Y0 = (U + D) / 2;
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            // the location of the next window Batman should jump to.
+            Console.WriteLine(X0 + " " + Y0);
+        }
+    }
+}
